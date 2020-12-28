e664f22a816fb85bc6c7ec2fef8c122e12350b18
Claudio Nogueira <cllaudionoggueira@gmail.com>
2020-12-28 01:48:33 -0300

    First commit

---
+++ Set02/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Entities {
+    class User {
+        public int Code { get; set; }
+
+        public User(int code) {
+            this.Code = code;
+        }
+
+        public override int GetHashCode() {
+            return Code.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is User)) {
+                return false;
+            }
+            User other = obj as User;
+            return Code.Equals(other.Code);
+        }
+    }
+}
---
+++ Set02/Program.cs
@@ -0,0 +1,48 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Set02 {
+    class Program {
+        static void Main(string[] args) {
+            HashSet<User> setA = new HashSet<User>();
+            HashSet<User> setB = new HashSet<User>();
+            HashSet<User> setC = new HashSet<User>();
+            try {
+                Console.Write("How many students for course A: ");
+                int qtdStudents = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= qtdStudents; i++) {
+                    Console.Write("Enter the code of the " + i + "# student: ");
+                    int codeStudent = int.Parse(Console.ReadLine());
+                    setA.Add(new User(codeStudent));
+                }
+
+                Console.Write("\nHow many students for course B: ");
+                qtdStudents = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= qtdStudents; i++) {
+                    Console.Write("Enter the code of the " + i + "# student: ");
+                    int codeStudent = int.Parse(Console.ReadLine());
+                    setB.Add(new User(codeStudent));
+                }
+
+                Console.Write("\nHow many students for course A: ");
+                qtdStudents = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= qtdStudents; i++) {
+                    Console.Write("Enter the code of the " + i + "# student: ");
+                    int codeStudent = int.Parse(Console.ReadLine());
+                    setC.Add(new User(codeStudent));
+                }
+
+                HashSet<User> users = new HashSet<User>();
+                users.UnionWith(setA);
+                users.UnionWith(setB);
+                users.UnionWith(setC);
+
+                Console.Write("\n\nTotal users: " + users.Count);
+            }
+            catch (FormatException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
